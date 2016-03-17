1f1c5ad2b72521a69f2f466f986e3f208268ae17
erfilov <admin@iMac.local>
2016-03-17 23:11:20 +0600

    Installing AFNetworking with pods

---
+++ FlickrMap/FlickrMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrMap
+//
+//  Created by Vik on 17.03.16.
+//  Copyright © 2016 Viktor Erfilov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlickrMap/FlickrMap/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  FlickrMap
+//
+//  Created by Vik on 17.03.16.
+//  Copyright © 2016 Viktor Erfilov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+
+
+
+@end
